a6b17fdc80a9b41d61a091652c86e748b4d56ff1
hariz0212 <194072635+hariz0212@users.noreply.github.com>
2025-11-29 13:47:47 +0100

    nav probleme

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
-import Nav from './compenent/nav'
+import Nav from './compenent/Nav'
 import Acceuil from "./Pages/Acceuil";
 import Projets from "./Pages/PageProjet"
 
